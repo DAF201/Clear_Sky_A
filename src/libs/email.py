503a0493af7550622d4379338351d3f0d8a47251
DAF201 <daf201@blink-in.com>
2023-09-29 10:05:44 -0500

    test

--- src/libs/email.py
+++ src/libs/email.py
@@ -1,24 +1,19 @@
 import smtplib
 import ssl
+from email_config import *
 
-smtp_server = "smtp.gmail.com"
-port = 587  # For starttls
-sender_email = "my@gmail.com"
-password = input("Type your password and press enter: ")
 
-# Create a secure SSL context
 context = ssl.create_default_context()
 
-# Try to log in to server and send email
+
 try:
-    server = smtplib.SMTP(smtp_server, port)
+    server = smtplib.SMTP(SMTP_SERVER, EMAIL_PORT)
+    server.ehlo()
+    server.starttls(context=context)
     server.ehlo()  # Can be omitted
-    server.starttls(context=context)  # Secure the connection
-    server.ehlo()  # Can be omitted
-    server.login(sender_email, password)
-    # TODO: Send email here
+    server.login(SENDER_EMAIL, SENDER_PASSWORD)
+    
 except Exception as e:
-    # Print any error messages to stdout
     print(e)
 finally:
     server.quit()
